5110389f718730395539ef5707407680ff6c114e
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2024-12-19 22:40:15 +0100

    Python file conversion script shenanigans.

--- src/convertFilesToWaw.py
+++ src/convertFilesToWaw.py
@@ -1,24 +1,23 @@
 import os
 
-def padSpaces(inp):
+
+def sanitize_file_name(inp):
     out = []
     inp = list(inp)
-    for char in inp:        
-        if char == " " or char == "&":
-            out.append("\\")
-
-        out.append(char)
+    for char in inp:
+        if char not in " %()":
+            out.append(char)
     return "".join(out)
 
-path = "../tracks"
 
-for filename in os.listdir(path):
-    if(not filename.endswith(".mp3")):
-        continue;
-    print("Converting ",filename)
-    name = filename.split(".mp3")[0]
-    name = padSpaces(name)
-    command = "mpg123 -w ../tracks/" + name + ".waw ../tracks/" + padSpaces(filename)
+PATH = "../tracks"
+
+for filename in os.listdir(PATH):
+    if (not filename.endswith(".mp3")):
+        continue
+    print("Converting ", filename)
+    sanitized_name = sanitize_file_name(filename.split(".mp3")[0])
+    command = f'mpg123 -w "{PATH}/{sanitized_name}.waw" "{PATH}/{filename}"'
     print("Command:", command)
     os.system(command)
-    os.system("rm ../tracks/" + padSpaces(filename))
+    os.system(f'rm "{PATH}/{filename}"')
